d83a4cfda65d5c83d3fa1298c296dca9debe9ebf
Vadim Persidskii <vadim@example.com>
2025-03-06 17:41:41 +0300

    Implement exception handlers

--- src/main/java/ru/netology/springBootRest/controller/AuthorizationController.java
+++ src/main/java/ru/netology/springBootRest/controller/AuthorizationController.java
@@ -1,9 +1,15 @@
 package ru.netology.springBootRest.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.springBootRest.domain.Authorities;
+import ru.netology.springBootRest.exception.InvalidCredentials;
+import ru.netology.springBootRest.exception.UnauthorizedUser;
 import ru.netology.springBootRest.service.AuthorizationService;
 
 import java.util.List;
@@ -19,5 +25,16 @@ public class AuthorizationController {
     @GetMapping("/authorize")
     public List<Authorities> getAuthorities(@RequestParam("user") String user, @RequestParam("password") String password) {
         return service.getAuthorities(user, password);
+    }
+
+    @ExceptionHandler(InvalidCredentials.class)
+    public ResponseEntity<String> invalidCredentialsHandler(InvalidCredentials e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnauthorizedUser.class)
+    public ResponseEntity<String> unauthorizedUserHandler(UnauthorizedUser e) {
+        System.out.println(e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 }
